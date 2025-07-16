3a01ee0eff6d4c42a6d942c8bae6efdcc52213a4
raiyan12999 <raiyanbinatik.m29@gmail.com>
2025-07-16 08:31:08 -0700

    Added task creating functionality

--- src/main/java/com/taskmanager/service/TaskService.java
+++ src/main/java/com/taskmanager/service/TaskService.java
@@ -1,14 +1,21 @@
 package com.taskmanager.service;
 
 import org.springframework.stereotype.Service;
+import com.taskmanager.model.Task;
 
 @Service
 public class TaskService {
     public String getTaskMessage(){
-        return "Task fetched from Service";
+        return "Tasks fetched from Service";
     }
 
     public String getWelcomeMessage(){
         return "Welcome from Task Manager";
+    }
+
+    public Task createTask(Task task){
+        // Simulate saving Task
+
+        return new Task(task.getId(), task.getTitle(), task.getDescription(), task.isCompleted());
     }
 }
